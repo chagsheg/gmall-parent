e926f0a15559cb0ea4897f6333840eb5098b5e3d
changsheng <1420952288@qq.com>
2021-05-19 23:37:06 +0800

    1

--- gmall-logger/src/main/java/com/atguigu/realtime/gmalllogger/GmallLoggerApplication.java
+++ gmall-logger/src/main/java/com/atguigu/realtime/gmalllogger/GmallLoggerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GmallLoggerApplication {
 // dfsd
     public static void main(String[] args) {
+        System.out.println("dfsdf ");
+
         SpringApplication.run(GmallLoggerApplication.class, args);
     }
 
